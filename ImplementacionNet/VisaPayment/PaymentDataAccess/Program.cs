00d6dba4c91e51af5f9135d181b11347f915044b
Reshi01 <danieldanih97@gmail.com>
2021-05-27 18:59:43 -0500

    Proyectos creados, modelos cargados

---
+++ ImplementacionNet/VisaPayment/PaymentDataAccess/Program.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using DataAccessService;
+
+namespace PaymentDataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new DataAccessor.DataAccessorClient(channel);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
