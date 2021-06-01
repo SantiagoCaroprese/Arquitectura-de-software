af620e3afbecd2dd71d39d101a935bcbe53d54a9
Reshi01 <danieldanih97@gmail.com>
2021-05-31 22:33:58 -0500

    esquemas de vistas

--- ImplementacionJEE/Presentacion/Presentacion/src/main/java/controladores/cliente/ControladorObtenerProductosCatalogo.java
+++ ImplementacionJEE/Presentacion/Presentacion/src/main/java/controladores/cliente/ControladorObtenerProductosCatalogo.java
@@ -1,10 +1,28 @@
 package controladores.cliente;
 
+import java.util.ArrayList;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.view.ViewScoped;
+
+import entidadesCatalogo.Producto;
+import modelos.catalogo.ObtenerProductosCatalogoBean;
 
 @ManagedBean
 @ViewScoped
 public class ControladorObtenerProductosCatalogo {
+	
+	private ArrayList<Producto> productos;
 
+	public ControladorObtenerProductosCatalogo() {
+	}
+	
+	public String verCatalogo() {
+		ObtenerProductosCatalogoBean bean= new ObtenerProductosCatalogoBean();
+		productos= bean.obtenerCatalogo();
+		if(productos==null || productos.isEmpty()) {
+			return "false";
+		}
+		return "true";
+	}
 }
--- ImplementacionJEE/Presentacion/Presentacion/src/main/java/modelos/catalogo/ObtenerProductosCatalogoBean.java
+++ ImplementacionJEE/Presentacion/Presentacion/src/main/java/modelos/catalogo/ObtenerProductosCatalogoBean.java
@@ -1,12 +1,32 @@
 package modelos.catalogo;
 
+import java.util.ArrayList;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.view.ViewScoped;
+
+import entidadesCatalogo.Producto;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Invocation;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.GenericType;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 
 public class ObtenerProductosCatalogoBean {
 
 	public ObtenerProductosCatalogoBean() {
+	}
+	
+	public ArrayList<Producto> obtenerCatalogo() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://www.myserver.com/book");
+		Invocation invocation = target.request(MediaType.APPLICATION_JSON).buildGet();
+		Response response = invocation.invoke();
+		ArrayList<Producto> productos=response.readEntity(new GenericType<ArrayList<Producto>>() {});
+		return productos;
 	}
 
 }
